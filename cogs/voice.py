ccf232460a8b75f2b36ef54ddf1e34887b09c962
Andriy <114863893+noinsts@users.noreply.github.com>
2025-02-27 16:08:33 +0200

    add new func: .afk (member_name)

--- cogs/voice.py
+++ cogs/voice.py
@@ -8,9 +8,19 @@ class Voice(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    """
-    COMMANDS HERE
-    """
+
+    @commands.command()
+    async def afk(self, ctx, member: discord.Member):
+        guild = self.bot.get_guild(cfg.GUILD_ID)
+        afk = guild.get_channel(cfg.AFK_ID)
+        voice = member.voice
+
+        if voice is None:
+            await ctx.send("Помилка, цієї людини немає в войсі")
+        else:
+            await member.move_to(afk)
+            await ctx.send(f"**{member.name}** відправлений до **{afk.name}**")
+
 
 
 async def setup(bot):
