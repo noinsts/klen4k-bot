ab1400188752b69bffba7023d674908bec60aed2
Andriy <114863893+noinsts@users.noreply.github.com>
2025-03-01 16:31:46 +0200

    New: .nickname (member.mention) (nickname/None)

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -23,6 +23,17 @@ class Moderation(commands.Cog):
 		await ctx.send(f"Користувач **{member}** розблокований")
 
 
+	@commands.command()
+	@commands.has_permissions(administrator = True)
+	async def nickname(self, ctx, member: discord.Member, newnick: str = None):
+		if newnick is None:
+			await member.edit(nick=None)
+			await ctx.send(f'Нікнейм скинуто для користувача **{member.display_name}**')
+		else:
+			await member.edit(nick=newnick)
+			await ctx.send(f'Нік **{newnick}** для користувача **{member.display_name}** встановлено')
+
+
 async def setup(bot):
 	await bot.add_cog(Moderation(bot))
 
